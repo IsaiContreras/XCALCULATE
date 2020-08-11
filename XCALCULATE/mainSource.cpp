807357f19b5815aa92f9c0e3c05acb1797a7735f
Isai_C221 <alexis_5476@hotmail.com>
2020-08-10 19:35:19 -0500

    DES. Ventana Matrices Compuestas.

--- XCALCULATE/mainSource.cpp
+++ XCALCULATE/mainSource.cpp
@@ -12,9 +12,29 @@
 #define BTN_SUB	 107
 #define BTN_MULT 108
 #define BTN_CLEAN 109
+#define EDT_MATRIXC 110
+#define EDT_TX 111
+#define EDT_TY 112
+#define EDT_TZ 113
+#define EDT_RX 114
+#define EDT_RY 115
+#define EDT_RZ 116
+#define EDT_SX 117
+#define EDT_SY 118
+#define EDT_SZ 119
+#define EDT_RPOINTS 120
+#define LBX_POINTS 121
+#define BTN_RESTART 122
+#define BTN_ADDP 123
+#define BTN_DELP 124
+#define BTN_TRANS 125
+#define BTN_ROTAT 126
+#define BTN_SCALA 127
+#define BTN_CALCULATE 128
 
 HWND hBtnArit;
 HWND hBtnComp;
+//AritmeticMenu
 HWND hEdtMatrix1;
 HWND hEdtMatrix2;
 HWND hEdtMatrix3;
@@ -25,6 +45,41 @@ HWND hBtnClean;
 HWND hStA1;
 HWND hStA2;
 HWND hStA3;
+//CompositeMenu
+HWND hEdtCompMatrix;
+HWND hEdtTX;
+HWND hEdtTY;
+HWND hEdtTZ;
+HWND hEdtRX;
+HWND hEdtRY;
+HWND hEdtRZ;
+HWND hEdtSX;
+HWND hEdtSY;
+HWND hEdtSZ;
+HWND hEdtRPoints;
+HWND hLbxPoints;
+HWND hBtnRestart;
+HWND hBtnAddP;
+HWND hBtnDelP;
+HWND hBtnTrans;
+HWND hBtnRotat;
+HWND hBtnScala;
+HWND hBtnCalculate;
+HWND hStM;
+HWND hStT;
+HWND hStT1;
+HWND hStT2;
+HWND hStT3;
+HWND hStR;
+HWND hStR1;
+HWND hStR2;
+HWND hStR3;
+HWND hStS;
+HWND hStS1;
+HWND hStS2;
+HWND hStS3;
+HWND hStP;
+HWND hStRP;
 
 class Matrix {
 	float** matrix = NULL;
@@ -190,11 +245,270 @@ void DestroyAritmeticMenu() {
 	hEdtMatrix1 = hEdtMatrix2 = hEdtMatrix3 = hBtnAdd = hBtnSub = hBtnMult = NULL;
 }
 
-void CreateCompoundMatrixMenu(HWND hWindow) {
+void CreateCompositeMatrixMenu(HWND hWindow) {
 	EnableWindow(hBtnComp, false); EnableWindow(hBtnArit, true);
+	hStM = CreateWindow(
+		"STATIC",
+		"Matriz Compuesta",
+		WS_CHILD | WS_VISIBLE | ES_CENTER,
+		10, 40, 350, 20, hWindow, NULL, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hEdtCompMatrix = CreateWindowEx(
+		0, "EDIT",
+		NULL,
+		WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_CENTER | ES_MULTILINE | ES_READONLY,
+		10, 60, 350, 175,
+		hWindow, (HMENU)EDT_MATRIXC, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hBtnRestart = CreateWindowEx(
+		0, "BUTTON",
+		"Reiniciar Matriz",
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+		10, 240, 350, 25,
+		hWindow, (HMENU)BTN_RESTART, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hStT = CreateWindow(
+		"STATIC",
+		"Traslación",
+		WS_CHILD | WS_VISIBLE | ES_LEFT,
+		10, 280, 100, 20, hWindow, NULL, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hEdtTX = CreateWindowEx(
+		0, "EDIT",
+		NULL,
+		WS_CHILD | WS_VISIBLE | ES_LEFT,
+		10, 300, 86, 25,
+		hWindow, (HMENU)EDT_TX, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hEdtTY = CreateWindowEx(
+		0, "EDIT",
+		NULL,
+		WS_CHILD | WS_VISIBLE | ES_LEFT,
+		98, 300, 86, 25,
+		hWindow, (HMENU)EDT_TY, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hEdtTZ = CreateWindowEx(
+		0, "EDIT",
+		NULL,
+		WS_CHILD | WS_VISIBLE | ES_LEFT,
+		186, 300, 86, 25,
+		hWindow, (HMENU)EDT_TZ, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hBtnTrans = CreateWindowEx(
+		0, "BUTTON",
+		"Aplicar",
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+		274, 300, 86, 25,
+		hWindow, (HMENU)BTN_TRANS, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hStT1 = CreateWindow(
+		"STATIC",
+		"x",
+		WS_CHILD | WS_VISIBLE | ES_CENTER,
+		10, 325, 86, 20, hWindow, NULL, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hStT2 = CreateWindow(
+		"STATIC",
+		"y",
+		WS_CHILD | WS_VISIBLE | ES_CENTER,
+		98, 325, 86, 20, hWindow, NULL, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hStT3 = CreateWindow(
+		"STATIC",
+		"z",
+		WS_CHILD | WS_VISIBLE | ES_CENTER,
+		186, 325, 86, 20, hWindow, NULL, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hStR = CreateWindow(
+		"STATIC",
+		"Rotación",
+		WS_CHILD | WS_VISIBLE | ES_LEFT,
+		10, 350, 100, 20, hWindow, NULL, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hEdtRX = CreateWindowEx(
+		0, "EDIT",
+		NULL,
+		WS_CHILD | WS_VISIBLE | ES_LEFT,
+		10, 370, 86, 25,
+		hWindow, (HMENU)EDT_RX, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hEdtRY = CreateWindowEx(
+		0, "EDIT",
+		NULL,
+		WS_CHILD | WS_VISIBLE | ES_LEFT,
+		98, 370, 86, 25,
+		hWindow, (HMENU)EDT_RY, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hEdtRZ = CreateWindowEx(
+		0, "EDIT",
+		NULL,
+		WS_CHILD | WS_VISIBLE | ES_LEFT,
+		186, 370, 86, 25,
+		hWindow, (HMENU)EDT_RZ, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hBtnRotat = CreateWindowEx(
+		0, "BUTTON",
+		"Aplicar",
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+		274, 370, 86, 25,
+		hWindow, (HMENU)BTN_ROTAT, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hStR1 = CreateWindow(
+		"STATIC",
+		"x",
+		WS_CHILD | WS_VISIBLE | ES_CENTER,
+		10, 395, 86, 20, hWindow, NULL, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hStR2 = CreateWindow(
+		"STATIC",
+		"y",
+		WS_CHILD | WS_VISIBLE | ES_CENTER,
+		98, 395, 86, 20, hWindow, NULL, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hStR3 = CreateWindow(
+		"STATIC",
+		"z",
+		WS_CHILD | WS_VISIBLE | ES_CENTER,
+		186, 395, 86, 20, hWindow, NULL, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hStS = CreateWindow(
+		"STATIC",
+		"Escalamiento",
+		WS_CHILD | WS_VISIBLE | ES_LEFT,
+		10, 420, 100, 20, hWindow, NULL, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hEdtSX = CreateWindowEx(
+		0, "EDIT",
+		NULL,
+		WS_CHILD | WS_VISIBLE | ES_LEFT,
+		10, 440, 86, 25,
+		hWindow, (HMENU)EDT_SX, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hEdtSY = CreateWindowEx(
+		0, "EDIT",
+		NULL,
+		WS_CHILD | WS_VISIBLE | ES_LEFT,
+		98, 440, 86, 25,
+		hWindow, (HMENU)EDT_SY, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hEdtSZ = CreateWindowEx(
+		0, "EDIT",
+		NULL,
+		WS_CHILD | WS_VISIBLE | ES_LEFT,
+		186, 440, 86, 25,
+		hWindow, (HMENU)EDT_SZ, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hBtnScala = CreateWindowEx(
+		0, "BUTTON",
+		"Aplicar",
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+		274, 440, 86, 25,
+		hWindow, (HMENU)BTN_SCALA, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hStS1 = CreateWindow(
+		"STATIC",
+		"x",
+		WS_CHILD | WS_VISIBLE | ES_CENTER,
+		10, 465, 86, 20, hWindow, NULL, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hStS2 = CreateWindow(
+		"STATIC",
+		"y",
+		WS_CHILD | WS_VISIBLE | ES_CENTER,
+		98, 465, 86, 20, hWindow, NULL, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hStS3 = CreateWindow(
+		"STATIC",
+		"z",
+		WS_CHILD | WS_VISIBLE | ES_CENTER,
+		186, 465, 86, 20, hWindow, NULL, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hStP = CreateWindow(
+		"STATIC",
+		"Puntos",
+		WS_CHILD | WS_VISIBLE | ES_CENTER,
+		370, 40, 240, 20, hWindow, NULL, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hLbxPoints = CreateWindowEx(
+		0, "LISTBOX",
+		NULL,
+		LBS_DISABLENOSCROLL | LBS_HASSTRINGS | WS_CHILD | WS_OVERLAPPED | WS_VISIBLE | WS_VSCROLL,
+		370, 60, 240, 180,
+		hWindow, (HMENU)LBX_POINTS, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hBtnAddP = CreateWindowEx(
+		0, "BUTTON",
+		"Agregar",
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+		370, 240, 120, 25,
+		hWindow, (HMENU)BTN_ADDP, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hBtnDelP = CreateWindowEx(
+		0, "BUTTON",
+		"Borrar",
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+		490, 240, 120, 25,
+		hWindow, (HMENU)BTN_DELP, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hStRP = CreateWindow(
+		"STATIC",
+		"Puntos Resultantes",
+		WS_CHILD | WS_VISIBLE | ES_CENTER,
+		370, 280, 240, 20, hWindow, NULL, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hBtnCalculate = CreateWindowEx(
+		0, "BUTTON",
+		"Calcular Puntos",
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+		370, 300, 240, 25,
+		hWindow, (HMENU)BTN_CALCULATE, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
+	hEdtRPoints = CreateWindowEx(
+		0, "EDIT",
+		NULL,
+		WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_CENTER | ES_MULTILINE | ES_READONLY,
+		370, 325, 240, 205,
+		hWindow, (HMENU)EDT_RPOINTS, (HINSTANCE)GetWindowLongPtr(hWindow, GWLP_HINSTANCE), NULL
+	);
 }
-void DestroyCompoundMatrixMenu() {
-
+void DestroyCompositeMatrixMenu() {
+	DestroyWindow(hStM);
+	DestroyWindow(hEdtCompMatrix);
+	DestroyWindow(hBtnRestart);
+	DestroyWindow(hStT);
+	DestroyWindow(hEdtTX);
+	DestroyWindow(hEdtTY);
+	DestroyWindow(hEdtTZ);
+	DestroyWindow(hBtnTrans);
+	DestroyWindow(hStT1);
+	DestroyWindow(hStT2);
+	DestroyWindow(hStT3);
+	DestroyWindow(hStR);
+	DestroyWindow(hEdtRX);
+	DestroyWindow(hEdtRY);
+	DestroyWindow(hEdtRZ);
+	DestroyWindow(hBtnRotat);
+	DestroyWindow(hStR1);
+	DestroyWindow(hStR2);
+	DestroyWindow(hStR3);
+	DestroyWindow(hStS);
+	DestroyWindow(hEdtSX);
+	DestroyWindow(hEdtSY);
+	DestroyWindow(hEdtSZ);
+	DestroyWindow(hBtnScala);
+	DestroyWindow(hStS1);
+	DestroyWindow(hStS2);
+	DestroyWindow(hStS3);
+	DestroyWindow(hLbxPoints);
+	DestroyWindow(hBtnAddP);
+	DestroyWindow(hBtnDelP);
+	DestroyWindow(hStRP);
+	DestroyWindow(hBtnCalculate);
+	DestroyWindow(hEdtRPoints);
+	hStM = hEdtCompMatrix = hBtnRestart = hStT = hEdtTX = hEdtTY = hEdtTZ = hBtnTrans = hStT1 = hStT2 = hStT3 = NULL;
+	hStR = hEdtRX = hEdtRY = hEdtRZ = hBtnRotat = hStR1 = hStR2 = hStR3 = NULL;
+	hStS = hEdtSX = hEdtSY = hEdtSZ = hBtnScala = hStS1 = hStS2 = hStS3 = NULL;
+	hStP = hLbxPoints = hBtnAddP = hBtnDelP = hStRP = hBtnCalculate = hEdtRPoints = NULL;
 }
 
 void initializeMatrix(float** mat, short columns, short rows) {
@@ -314,12 +628,12 @@ LRESULT CALLBACK WinProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	case WM_COMMAND: {
 		switch (wParam) {
 		case BTN_ARIT:
-			DestroyCompoundMatrixMenu();
+			DestroyCompositeMatrixMenu();
 			CreateAritmeticMenu(hWnd);
 			break;
 		case BTN_COMP:
 			DestroyAritmeticMenu();
-			CreateCompoundMatrixMenu(hWnd);
+			CreateCompositeMatrixMenu(hWnd);
 			break;
 		case BTN_CLEAN:
 			SetWindowText(hEdtMatrix1, "");
